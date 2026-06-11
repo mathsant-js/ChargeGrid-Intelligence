45c7b8a56228679088721870ac2f823e59b49b29
Matheus Santana <130666046+mathsant-js@users.noreply.github.com>
2026-06-11 14:16:23 -0300

    refactor: adicionando função para calcular a distribuição da potência

--- app.py
+++ app.py
@@ -4,5 +4,19 @@ VALOR_KWH = 0.95
 def calcular_pagamento(energia_consumida):
     return energia_consumida * VALOR_KWH
 
-def calcular_demanda_total(demanda):
-    return sum(demanda)
+def calcular_demanda_total(demandas):
+    return sum(demandas)
+
+def distribuir_potencia(demandas):
+    demanda_total = calcular_demanda_total(demandas)
+    
+    distribuicao = []
+    
+    for demanda in demandas:
+        percentual = demanda / demanda_total
+        
+        potencia_recebida = percentual * POTENCIA_MAXIMA_ESTACAO
+        
+        distribuicao.append(potencia_recebida)
+    
+    return distribuicao
